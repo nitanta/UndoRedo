c7e0a75c86a9de71c40258a19c8208f05cb8d866
Nitanta Adhikari <nitanta@oozou.com>
2022-03-25 13:37:30 +0545

    Initial Commit

---
+++ Undomanager/UndomanagerApp.swift
@@ -0,0 +1,13 @@
+//Copyright © 2022 and Confidential to ___ORGANIZATIONNAME___ All rights reserved.
+   
+
+import SwiftUI
+
+@main
+struct UndomanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UndomanagerUITests/UndomanagerUITestsLaunchTests.swift
@@ -0,0 +1,28 @@
+//Copyright © 2022 and Confidential to ___ORGANIZATIONNAME___ All rights reserved.
+   
+
+import XCTest
+
+class UndomanagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
